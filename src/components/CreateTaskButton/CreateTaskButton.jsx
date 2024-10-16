5d5ba856becf1db8fb81aa961fb3cc8ab665334b
PetrDidur <pdidur@yandex.ru>
2024-10-16 09:33:47 +0300

    all except cards

---
+++ src/components/CreateTaskButton/CreateTaskButton.jsx
@@ -0,0 +1,7 @@
+export default function CreateTaskButton () {
+  return (
+    <button className="header__btn-main-new _hover01" id="btnMainNew">
+            <a href="#popNewCard">Создать новую задачу</a>
+          </button>
+  )
+}
